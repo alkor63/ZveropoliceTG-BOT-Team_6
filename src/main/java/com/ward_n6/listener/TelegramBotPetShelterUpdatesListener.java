89323ea2bbb5fb60cdb0a44a5735635dbf96f556
Rumonz <lone_assassin@mail.ru>
2023-07-30 20:00:17 +0300

    Начал делать кнопки

--- src/main/java/com/ward_n6/listener/TelegramBotPetShelterUpdatesListener.java
+++ src/main/java/com/ward_n6/listener/TelegramBotPetShelterUpdatesListener.java
@@ -2,11 +2,15 @@ package com.ward_n6.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.CallbackQuery;
 import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.Keyboard;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
-import com.ward_n6.Keyboard.Bot;
+import com.vdurmont.emoji.EmojiParser;
 import com.ward_n6.entity.BotMessaging;
 import com.ward_n6.service.BotMessageService;
 import org.slf4j.Logger;
@@ -38,24 +42,75 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
         telegramBot.setUpdatesListener(this);
     }
 
-    private Bot bot;
     boolean startSelected = false; // переменная для подтверждения старта
 
     @Override
     public int process(List<Update> updates) {
         try {
-            updates.stream() // получаем поток апдейтов из листа
-                    .filter(update -> update.message() != null)// фильтруем по тем, что не нулл
+            updates
                     .forEach(update -> { // итерируемся по ним
                         logger.info("Processing update: {}", update); // записываем лог апдейтов на уровне инфо
+
+                                if (update.callbackQuery() != null) {
+                                    Long chatId = update.callbackQuery().message().chat().id();
+                                    CallbackQuery callbackQuery = update.callbackQuery();
+                                    String data = callbackQuery.data();
+                                    switch (data) {
+                                        case "КНОПКА_ПРИЮТ_ДЛЯ_СОБАК": {
+                                            SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для собак");
+                                            InlineKeyboardButton dogHouseInfoButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Информация о приюте "+":information_source:"));
+                                            dogHouseInfoButton.callbackData("ИНФО");
+                                            InlineKeyboardButton dogHouseHowToTakeButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Как взять собаку из приюта "+":dog:"));
+                                            dogHouseHowToTakeButton.callbackData("КАК_ЗАБРАТЬ_СОБАКУ");
+                                            InlineKeyboardButton dogHouseOwnerReportButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Прислать отчёт о питомце "+":memo:"));
+                                            dogHouseOwnerReportButton.callbackData("ОТЧЁТ");
+                                            InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser.parseToUnicode("Позвать волонтёра "+":necktie:"));
+                                            callVoluntier.callbackData("ВОЛОНТЁР");
+
+                                            Keyboard keyboard = new InlineKeyboardMarkup(dogHouseHowToTakeButton, dogHouseInfoButton, dogHouseOwnerReportButton,callVoluntier);
+
+                                            sendMessage.replyMarkup(keyboard);
+                                            telegramBot.execute(sendMessage);
+                                        break;}
+                                        case "КНОПКА_ПРИЮТ_ДЛЯ_КОШЕК" :{
+                                            SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для кошек");
+                                            InlineKeyboardButton catHouseInfoButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Информация о приюте "+":information_source:"));
+                                            catHouseInfoButton.callbackData("ИНФО");
+                                            InlineKeyboardButton catHouseHowToTakeButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Как взять собаку из приюта "+":cat:"));
+                                            catHouseHowToTakeButton.callbackData("КАК_ЗАБРАТЬ_СОБАКУ");
+                                            InlineKeyboardButton catHouseOwnerReportButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Прислать отчёт о питомце "+":memo:"));
+                                            catHouseOwnerReportButton.callbackData("ОТЧЁТ");
+                                            InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser.parseToUnicode("Позвать волонтёра "+":necktie:"));
+                                            callVoluntier.callbackData("ВОЛОНТЁР");
+
+                                            Keyboard keyboard = new InlineKeyboardMarkup().addRow(catHouseHowToTakeButton,catHouseInfoButton).addRow(catHouseOwnerReportButton,callVoluntier);
+
+                                            sendMessage.replyMarkup(keyboard);
+                                            telegramBot.execute(sendMessage);
+
+                                                break;}
+                                    } return;
+                                }
                         Message message = update.message(); // получаем сообщение из текущего обновления
                         Long chatId = message.chat().id(); // получаем идентификатор чата, к которому относится апдейт
                         String messageText = message.text(); // получаем текст сообщения
+
                         switch (messageText) {
                             case "/start":
                                 startSelected = true;
-                                sendMessage(chatId, "Здравствуйте. Это чатбот приюта для животных. " +
+                                SendMessage sendMessage = new SendMessage(chatId, "Здравствуйте. Это чатбот приюта для животных. " +
                                         "Какой приют Вас интересует?");
+                                com.pengrad.telegrambot.model.request.InlineKeyboardButton chooseDogHouseButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Приют для собак"+":dog:"));
+                                chooseDogHouseButton.callbackData("КНОПКА_ПРИЮТ_ДЛЯ_СОБАК");
+                                com.pengrad.telegrambot.model.request.InlineKeyboardButton chooseCatHouseButton = new InlineKeyboardButton(EmojiParser.parseToUnicode("Приют для кошек"+":cat:"));
+                                chooseCatHouseButton.callbackData("КНОПКА_ПРИЮТ_ДЛЯ_КОШЕК");
+
+                                Keyboard keyboard = new InlineKeyboardMarkup(chooseDogHouseButton,chooseCatHouseButton);
+
+
+                                sendMessage.replyMarkup(keyboard);
+                                telegramBot.execute(sendMessage);
+
                                 break;
                             case "/dogs":
                                 if (startSelected) {
@@ -87,6 +142,8 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     }
 
 
+
+
     private void saveMessages(long chatId, String messageText) {
         if (messageText.isEmpty()) { // обрабатываем нулловое значение из парсинга
             sendMessage(chatId, "Неверный формат сообщения");
@@ -101,7 +158,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
 
 
     // ОТПРАВКА ОТВЕТА БОТА:
-    private void sendMessage(Long chatId, String message) { // выносим отправку в отдельный метод
+    private void sendMessage(long chatId, String message) { // выносим отправку в отдельный метод
         SendMessage sendMessage = new SendMessage(chatId, message);
 //        sendMessage.replyMarkup(replyKeyboardMarkup);
         SendResponse sendResponse = telegramBot.execute(sendMessage); // сохраняем в переменную sendMessage
