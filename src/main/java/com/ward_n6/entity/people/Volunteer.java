260c9247054e4fcb9a0001ad1b6cd25fc5e49284
alkor63 <alkor@medi-spa.ru>
2023-08-02 02:00:41 +0300

    создал сервисные классы для 3 таблиц Наполнил их CRUD-методами, использовал эти методы в Волонтёрах Добавил Волонтёрам еще пару методов

--- src/main/java/com/ward_n6/entity/people/Volunteer.java
+++ src/main/java/com/ward_n6/entity/people/Volunteer.java
@@ -76,5 +76,71 @@ public class Volunteer {
         if (num > 0) System.out.println("все отчеты обработаны");
         return num; // количество отчетов за 24 часа до 21:00 указанной даты
     }
+
+    public int endOfProbationPeriod(PetWithOwner petWithOwner) {
+
+// Исходим из того, что запрос на "приговор" приходит в день окончания испытательного срока
+// т.е. дата = localDate.now()
+// просматриваем все отчеты этого усыновителя за испытательный период и присваиваем ему рейтинг
+        long petId = petWithOwner.getPet().getId();
+        int numReport = 0;
+        int numOldReport = 0;
+        LocalDate today = LocalDate.now();
+        LocalDate before30 = today.minusDays(30);
+        LocalDate before60 = today.minusDays(61);
+        // list всех отчетов
+        List<OwnerReport> ownerReportList = new ArrayList<>();
+        // foreach для всех отчетов
+        for (OwnerReport ownerReport : ownerReportList) {
+            LocalDate date = ownerReport.getReportDateTime().toLocalDate();
+            if (ownerReport.getPetId() ==  petId) {
+                if(date.isBefore(before30) && date.isAfter(before60)) {
+                    numOldReport++;
+                    break;
+                }
+                if (date.isAfter(before30.minusDays(1))) numReport++;
+            }
+        }
+        if (numReport == 0) return 0;
+        else if (numReport <= 10 && numOldReport == 0) return 1;
+        else if (numReport <= 20 && numOldReport == 0) return 2;
+        else return 3;
+    }
+
+    // метод обработки "приговора", вызывающий endOfProbationPeriod() - перенести в listener Боту
+    public void ownersVerdict(PetWithOwner petWithOwner) throws PutToMapException, DeleteFromMapException {
+        int rating = endOfProbationPeriod(petWithOwner);
+        switch (rating) {
+            case 0:
+                // вернуть животное в приют
+                System.out.println("За 30 дней Вы ни разу не прислали отчет. Вы должны вернуть животное в приют!");
+                //         petsOwnerService.deletePetWithOwnerByValue(petWithOwner); ==> удалить запись из таблицы
+                break;
+            case 1:
+                // продлить испытательный срок на 30 дней
+                System.out.println("Вы очень редко присылали отчетыю Испытательный срок продлен на 30 дней");
+                LocalDate newEndDate30 = LocalDate.now().plusDays(30);
+                petWithOwner.setEndDate(newEndDate30);   // узнать его id
+                petsOwnerService.editPetWithOwnerById();  //лучше по value
+                        PetWithOwner editPetWithOwnerById(int recordId, PetWithOwner petWithOwner)
+                break;
+            case 2:
+                // продлить испытательный срок на 14 дней
+                System.out.println("Вы присылали отчеты не регулярно. Испытательный срок продлен на 14 дней");
+                LocalDate newEndDate14 = LocalDate.now().plusDays(14);
+                petWithOwner.setEndDate(newEndDate30);   // узнать его id
+
+                break;
+            case 3:
+                // поздравить с успешным прохождением испытательношо срока
+                System.out.println("Поздравляем с завершением испытательного срока! Ваш питомец остаётся с Вами навсегда");
+                PetWithOwner petToOwner = removePetWithOwnerToArchive(petWithOwner);
+//                delete pet from pet_table
+                break;
+            default:
+                System.out.println("Некорректное значение rating. Разработчик что-то накосячил");
+        }
+    }
+
 }
 
