83c769dade97ae843fbcff396a9cf09ad55eacce
alkor63 <alkor@medi-spa.ru>
2023-08-02 16:58:43 +0300

    Написал PetController для Pet Аналогично OwnerController для Owner

---
+++ src/main/java/com/ward_n6/service/OwnerService.java
@@ -0,0 +1,28 @@
+package com.ward_n6.service;
+
+import com.ward_n6.entity.owners.Owner;
+import com.ward_n6.service.Impl.DeleteFromMapException;
+import com.ward_n6.service.Impl.EditMapException;
+import com.ward_n6.service.Impl.PutToMapException;
+
+import java.util.List;
+
+public interface OwnerService {
+    int getId();
+
+    Owner addOwner(Owner owner) throws PutToMapException;
+
+    Owner getOwnerById(int recordId);
+
+    List<Owner> getAllOwners();
+
+    Owner editOwnerById(int recordId, Owner owner) throws EditMapException;
+
+    void deleteAllFromOwner();
+
+    boolean deleteOwnerById(int recordId) throws DeleteFromMapException;
+
+    boolean deleteOwnerByValue(Owner owner) throws DeleteFromMapException;
+
+    int idOwnerByValue(Owner owner);
+}
