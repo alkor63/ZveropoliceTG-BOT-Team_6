6e53f698242f916b7250fa4e7dd74082145ac2aa
Liz <eliza.an.popova@gmail.com>
2023-08-19 22:36:15 +0300

    Тесты для выведения ошибок для контроллера Owner

--- src/test/java/com/ward_n6/OwnerAndPetConstants.java
+++ src/test/java/com/ward_n6/OwnerAndPetConstants.java
@@ -17,6 +17,7 @@ public class OwnerAndPetConstants {
     public static final String INFO = "Spring-Boot-приложение с функцией telegram-bot, skyPro Java_14, команда Палата_№6 (Анастасия, Алексей, Елизавета, Роман, Тимур), июль-август 2023г.";
     public static final Owner OWNER_1 = new Owner(1L, "Ivan", "Ivanov", "+79099999999");
     public static final Owner OWNER_2 = new Owner(2L, "Boris", "Petrov", "+7909888888");
+    public static final Owner INCORRECT_OWNER = new Owner(2L, "", "", "");
     public static final Pet PET_1 = new Pet (1, CAT, FEM, "Marusya", "19.05.2019", "britain");
     public static final Pet PET_2 = new Pet (1, DOG, MALE, "Sharik", "12.12.2022", "malamut");
     public static final List <Pet> LIST_OF_PETS = List.of(PET_1, PET_2);
