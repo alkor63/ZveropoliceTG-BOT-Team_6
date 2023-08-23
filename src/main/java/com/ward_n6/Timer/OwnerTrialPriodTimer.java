2626d460fa47cfce51d4cfe48df260dc9ea72d94
alkor63 <alkor@medi-spa.ru>
2023-08-23 21:21:26 +0300

    Победил все тесты в VolunteerServiceTest !!!

--- src/main/java/com/ward_n6/Timer/OwnerTrialPriodTimer.java
+++ src/main/java/com/ward_n6/Timer/OwnerTrialPriodTimer.java
@@ -1,5 +1,6 @@
 package com.ward_n6.Timer;
 import com.pengrad.telegrambot.TelegramBot;
+import com.pengrad.telegrambot.request.SendMessage;
 import com.ward_n6.entity.owners.Owner;
 import com.ward_n6.entity.owners.PetsOwner;
 import com.ward_n6.entity.owners.PetsOwnerArchive;
@@ -43,28 +44,39 @@ public class OwnerTrialPriodTimer {
 
 
 //    @Scheduled(cron = "* 00 21 * * *") //вызов каждый день в 21:00
-    @Scheduled(cron = "* 45 20 * * *") //вызов каждый день в мм чч на время отладки
+    @Scheduled(cron = "* 12 23 * * *") //вызов каждый день в мм чч на время отладки
 
     public void task() {
-        //проверяем метод Позвать волонтёра
-        System.out.println(volunteerService.callVolunteer("Хочу собачку"));
+        long chatId = 1L;  // заменить на owner.getId()
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        //проверяем метод Позвать волонтёра
+        System.out.println(volunteerService.callVolunteer("Будущий лучший хозяин"));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         // Волонтёр просматривает все отчеты за указанную дату
         String svr = volunteerService.viewAllReports(LocalDate.now());
         System.out.println(svr);
+//        telegramBot.execute(new SendMessage(chatId, svr));
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // просматриваем все отчёты, оцениваем качество каждого отчета
         // можно выбрать только отчёты с конкретным id животного и/или хозяина
         List<OwnerReport> allOwnerReports = ownerReportRepository.findAll();
         for (OwnerReport ownerReport : allOwnerReports) {
-            System.out.println("Verdict "+ownerReport.getId()+" = " + volunteerService.reportExpertise(ownerReport));
+            String verdict = "Verdict "+ownerReport.getId()+" = " + volunteerService.reportExpertise(ownerReport);
+            System.out.println(verdict);
+//            telegramBot.execute(new SendMessage(chatId, verdict));
         }
 
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
         List<PetsOwner> allPetOwners = petsOwnerRepository.findAll();
         for (PetsOwner petsOwner : allPetOwners) {
             if (petsOwner.getDateEnd().equals(LocalDate.now())) {
 //если сегодня день окончания испытательного срока, Волонтёр выносит "приговор"
-                String verdict = volunteerService.ownersVerdict(petsOwner);
-                System.out.println("Сообщение для хозяина с id= " + petsOwner.getOwnerId() + " :/n" + verdict);
+                String finalVerdict = volunteerService.ownersVerdict(petsOwner);
+//                telegramBot.execute(new SendMessage(chatId, finalVerdict));
+                System.out.println("Сообщение для хозяина с id= " + petsOwner.getOwnerId() + " :/n" + finalVerdict);
             }
         }
         return;
