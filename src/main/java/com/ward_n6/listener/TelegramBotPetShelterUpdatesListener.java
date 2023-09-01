ced488a347809914301a1bd1747f668b0dfcf0b2
Rumonz <lone_assassin@mail.ru>
2023-09-02 00:15:06 +0300

    пуш просто для сохранения файлов

--- src/main/java/com/ward_n6/listener/TelegramBotPetShelterUpdatesListener.java
+++ src/main/java/com/ward_n6/listener/TelegramBotPetShelterUpdatesListener.java
@@ -16,6 +16,8 @@ import com.vdurmont.emoji.EmojiParser;
 import com.ward_n6.entity.BotMessaging;
 import com.ward_n6.entity.Photo;
 import com.ward_n6.entity.owners.Owner;
+import com.ward_n6.entity.shelters.CatsShelter;
+import com.ward_n6.entity.shelters.DogsShelter;
 import com.ward_n6.entity.shelters.PetShelter;
 import com.ward_n6.repository.PhotoRepository;
 import com.ward_n6.repository.pets.CatRepository;
@@ -44,6 +46,8 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     private final PetsOwnerServiceImpl petsOwnerServiceImpl;
     private final PetShelter petShelter = new PetShelter() {
     };
+    private final DogsShelter dogsShelter = new DogsShelter(){};
+    private final CatsShelter catsShelter = new CatsShelter(){};
 
     private final ReportService reportService;
 
@@ -145,6 +149,12 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
                         case "ОТЧЁТ":
                             sendOwnerHowReport(chatId);
                             break;
+                        case "РЕКОМЕНДАЦИЯ_СОБАКИ":
+                            dogRecommendation(chatId);
+                            break;
+                        case "РЕКОМЕНДАЦИЯ_КОШКИ":
+                            catRecommendation(chatId);
+                            break;
                     }
                 }
 
@@ -156,7 +166,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
                 switch (messageText) {
                     case "/start":
                         startSelected = true;
-                        afterStartMenu(chatId, "/start");
+                        afterStartMenu(chatId);
                         break;
                     case "/dogs":
                         dogSelect = true;
@@ -289,7 +299,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     //МЕТОДЫ ДЛЯ КНОПОК:
 
     //После старта:
-    private void afterStartMenu(long chatId, String message) {
+    private void afterStartMenu(long chatId) {
         SendMessage sendMessage = new SendMessage(chatId, "Здравствуйте. " +
                 "Это чат-бот приюта для животных. " +
                 "Какой приют Вас интересует?");
@@ -306,7 +316,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
 
     //КОШКИ:
     private void catButton(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для кошек");
+        SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для кошек! \n"+ catsShelter.getDescriptionCat());
         InlineKeyboardButton catHouseInfoButton = new InlineKeyboardButton(EmojiParser
                 .parseToUnicode("Информация о приюте " + ":information_source:"));
         catHouseInfoButton.callbackData("ИНФО_КОШКИ");
@@ -330,7 +340,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
 
     //СОБАКИ
     private void dogButton(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для собак");
+        SendMessage sendMessage = new SendMessage(chatId, "Вы выбрали приют для собак! \n"+dogsShelter.getDescriptionDog());
         InlineKeyboardButton dogHouseInfoButton = new InlineKeyboardButton(EmojiParser
                 .parseToUnicode("Информация о приюте " + ":information_source:"));
         dogHouseInfoButton.callbackData("ИНФО_СОБАКИ");
@@ -353,12 +363,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     }
 
     private void dogInfoButton(long chatId) {
-//        SendMessage sendMessage = new SendMessage(chatId, petShelter.getDescription()+"\n"+petShelter.getShelterAddress()+"\n"+petShelter.getSecurityContact()+"\n"+petShelter.getSafetyAdvice()+"\n"+petShelter.getHelpShelter());
-        SendMessage sendMessage = new SendMessage(chatId, "Мы приветствуем вас в приюте «Мечта». Наш приют основан в 2015 году в г. Астана. За это время через наши руки прошло более 500 собак и 200 кошек. Мы являемся некоммерческой организацией, поэтому все средства к нашему существованию – это пожертвования неравнодушных жителей. Мы очень рады, что вы заглянули к нам!\n" +
-                "«Мечта» и его жители находятся по адресу: ул. Защитников Животных д.1. Мы открыты для посещений каждый день с 11:00 до 18:00.\n\n" +
-                "Борис на связи +77894561230. Он поможет вам оформить пропуск, а также ответить на интересующие вас вопросы.\n\n" +
-                "Напоминаем, что во время нахождения на территории приюта посетители обязаны следовать указаниям сотрудников. Безопасность питомцев и посетителей в наших с вами руках.\n\n" +
-                "Приют нуждается в физической и материальной помощи. Требуются сотрудники для создания благоприятных условий. Приют нуждается в кормах и медикаментах, а также хозяйственных принадлежностях – тряпках, полотенцах, одеялах, амуниции и игрушек для животных. Пиар приветствуется. Помочь приюту можно переводом денежных средств по реквизитам карт в банках, номерам электронных кошельков. Для согласования оказания физической и материальной поддержки свяжитесь с волонтёром. Подробнее о нашей деятельности вы можете узнать в социальных сетях. Пожалуйста, присоединяйтесь к нашим сообществам Вконтакте, Одноклассники и т.д.");
+        SendMessage sendMessage = new SendMessage(chatId, petShelter.getDescription()+"\n"+petShelter.getDogShelterAddress()+"\n"+petShelter.getSecurityContact()+"\n"+petShelter.getSafetyAdvice()+"\n"+petShelter.getHelpShelter());
         InlineKeyboardButton wantToHelpButton = new InlineKeyboardButton(EmojiParser.parseToUnicode(":two_hearts: " + "Я хочу пожертвовать в приют ".toUpperCase() + ":two_hearts:"));
         wantToHelpButton.callbackData("СДЕЛАТЬ_ПОЖЕРТВОВАНИЕ");
         InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser
@@ -372,12 +377,7 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     }
 
     private void catInfoButton(long chatId) {
-//        SendMessage sendMessage = new SendMessage(chatId, petShelter.getDescription()+"\n"+petShelter.getShelterAddress()+"\n"+petShelter.getSecurityContact()+"\n"+petShelter.getSafetyAdvice()+"\n"+petShelter.getHelpShelter());
-        SendMessage sendMessage = new SendMessage(chatId, "Мы приветствуем вас в приюте «Мечта». Наш приют основан в 2015 году в г. Астана. За это время через наши руки прошло более 500 собак и 200 кошек. Мы являемся некоммерческой организацией, поэтому все средства к нашему существованию – это пожертвования неравнодушных жителей. Мы очень рады, что вы заглянули к нам!\n" +
-                "«Мечта» и его жители находятся по адресу: ул. Защитников Животных д.2. Мы открыты для посещений каждый день с 11:00 до 18:00.\n\n" +
-                "Борис на связи +77894561230. Он поможет вам оформить пропуск, а также ответить на интересующие вас вопросы.\n\n" +
-                "Напоминаем, что во время нахождения на территории приюта посетители обязаны следовать указаниям сотрудников. Безопасность питомцев и посетителей в наших с вами руках.\n\n" +
-                "Приют нуждается в физической и материальной помощи. Требуются сотрудники для создания благоприятных условий. Приют нуждается в кормах и медикаментах, а также хозяйственных принадлежностях – тряпках, полотенцах, одеялах, амуниции и игрушек для животных. Пиар приветствуется. Помочь приюту можно переводом денежных средств по реквизитам карт в банках, номерам электронных кошельков. Для согласования оказания физической и материальной поддержки свяжитесь с волонтёром. Подробнее о нашей деятельности вы можете узнать в социальных сетях. Пожалуйста, присоединяйтесь к нашим сообществам Вконтакте, Одноклассники и т.д.");
+        SendMessage sendMessage = new SendMessage(chatId, petShelter.getDescription()+"\n"+petShelter.getCatShelterAddress()+"\n"+petShelter.getSecurityContact()+"\n"+petShelter.getSafetyAdvice()+"\n"+petShelter.getHelpShelter());
         InlineKeyboardButton wantToHelpButton = new InlineKeyboardButton(EmojiParser.parseToUnicode(":two_hearts: " + "Я хочу пожертвовать в приют ".toUpperCase() + ":two_hearts:"));
         wantToHelpButton.callbackData("СДЕЛАТЬ_ПОЖЕРТВОВАНИЕ");
         InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser
@@ -391,54 +391,52 @@ public class TelegramBotPetShelterUpdatesListener implements UpdatesListener {
     }
 
     private void wantToTakeDogButton(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode("Рады, что вы заинтересованы стать обладателем собаки! Прежде чем оформить документы, необходимо познакомиться с питомцем. Встреча и общение с животным проходят в присутствии и под наблюдением ответственного сотрудника приюта. Для оформления договора потребуется ваш паспорт РФ. Испытательный срок составляет 30 дней. В течение этого срока хозяин обязан присылать ежедневные отчеты (в чат-бот, на электронную почту, на WhatsApp и пр.). Выбранный способ отчетности также фиксируется в договоре.\n\n " +
-                ":no_entry:Почему мы можем отказать в усыновлении?:no_entry:\n" +
-                ":one:\tЖивотное не пошло с вами на контакт.\n" +
-                ":two:\tУсловия будущего проживания с вами не соответствуют необходимым для конкретного питомца.\n" +
-                ":three:\tСемейные разногласия – нередко один член семьи готов завести питомца, а другой (другие) против. В таком случае мы отказываем.\n" +
-                ":four:\tОтказ усыновителя оформить документы и пройти испытательный срок. Желание ускорить процесс будет для нас «красным маркером».\n" +
-                ":five:\tЧасто мы отказываем в усыновлении, если в семье уже имеется какое-нибудь животное или несколько животных. В зависимости от характера нашего питомца, мы пристраиваем его в семьи без животных, либо с животными.\n"));
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(petShelter.getTakeDogPet()+"\n\n"+petShelter.getProhibitedTakePet() ));
         InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser
                 .parseToUnicode("Позвать волонтёра " + ":necktie:"));
         callVoluntier.callbackData("ВОЛОНТЁР");
+        InlineKeyboardButton recommendationDog = new InlineKeyboardButton(EmojiParser.parseToUnicode(":point_up: Рекомендации перед тем, как завести собаку! :point_up:"));
+        recommendationDog.callbackData("РЕКОМЕНДАЦИЯ_СОБАКИ");
         Keyboard keyboard = new InlineKeyboardMarkup()
+                .addRow(recommendationDog)
                 .addRow(callVoluntier);
         sendMessage.replyMarkup(keyboard);
         telegramBot.execute(sendMessage);
     }
 
     private void wantToTakeCatButton(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode("Рады, что вы заинтересованы стать обладателем кошки! Прежде чем оформить документы, необходимо познакомиться с питомцем. Встреча и общение с животным проходят в присутствии и под наблюдением ответственного сотрудника приюта. Для оформления договора потребуется ваш паспорт РФ. Испытательный срок составляет 30 дней. В течение этого срока хозяин обязан присылать ежедневные отчеты (в чат-бот, на электронную почту, на WhatsApp и пр.). Выбранный способ отчетности также фиксируется в договоре.\n\n " +
-                ":no_entry:Почему мы можем отказать в усыновлении?:no_entry:\n" +
-                ":one:\tЖивотное не пошло с вами на контакт.\n" +
-                ":two:\tУсловия будущего проживания с вами не соответствуют необходимым для конкретного питомца.\n" +
-                ":three:\tСемейные разногласия – нередко один член семьи готов завести питомца, а другой (другие) против. В таком случае мы отказываем.\n" +
-                ":four:\tОтказ усыновителя оформить документы и пройти испытательный срок. Желание ускорить процесс будет для нас «красным маркером».\n" +
-                ":five:\tЧасто мы отказываем в усыновлении, если в семье уже имеется какое-нибудь животное или несколько животных. В зависимости от характера нашего питомца, мы пристраиваем его в семьи без животных, либо с животными.\n"));
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(petShelter.getTakeCatPet()+"\n\n"+petShelter.getProhibitedTakePet()));
         InlineKeyboardButton callVoluntier = new InlineKeyboardButton(EmojiParser
                 .parseToUnicode("Позвать волонтёра " + ":necktie:"));
         callVoluntier.callbackData("ВОЛОНТЁР");
+        InlineKeyboardButton recommendationCat = new InlineKeyboardButton(EmojiParser.parseToUnicode(":point_up: Рекомендации перед тем, как завести кошку! :point_up:"));
+        recommendationCat.callbackData("РЕКОМЕНДАЦИЯ_КОШКИ");
         Keyboard keyboard = new InlineKeyboardMarkup()
+                .addRow(recommendationCat)
                 .addRow(callVoluntier);
         sendMessage.replyMarkup(keyboard);
         telegramBot.execute(sendMessage);
     }
 
     private void callVoluntier(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(":white_check_mark:Через " +
-                "некоторое время с Вами свяжется волонтёр и ответит на интересующие вопросы!:wink:"));
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(petShelter.getCallVolonteer()));
         telegramBot.execute(sendMessage);
+    }
+
+    private void dogRecommendation(long chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(dogsShelter.getTakeDogRecommendation()));
+        telegramBot.execute(sendMessage);
+
+    }
+    private void catRecommendation(long chatId) {
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(catsShelter.getTakeCatRecommendation()));
+        telegramBot.execute(sendMessage);
+
     }
 
 
     private void sendOwnerHowReport(long chatId) {
-        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode("""
-                Для отчёта нужно отправить текст , описывающий: самочувствие питомца, рацион питания питомца, в каких условиях живёт питомец и фото питомца 
-                        Для отправки текстового отчёта нажмите или введите команду:
-                       :memo: /report
-                         
-                        Для отправки фото нажмите или введите команду:
-                       :camera: /photo"""
+        SendMessage sendMessage = new SendMessage(chatId, EmojiParser.parseToUnicode(petShelter.getReportInfo()
         ));
 //        InlineKeyboardButton textButton = new InlineKeyboardButton(EmojiParser
 //                .parseToUnicode("Отправить текст :memo:"));
